5ccd73a680200218c9d735b6ffb150da889981e5
Anatoly <anatoly.strashkevich@gmail.com>
2019-07-24 15:15:45 +0300

    load

--- app/app.tsx
+++ app/app.tsx
@@ -2,40 +2,46 @@ import './assets/fonts/index.css';
 import './assets/styles.css'; 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { isEmpty, path, identity, reject } from 'ramda';
+import * as THREE from "three";
+import { isEmpty, identity, reject, all } from 'ramda';
 import { Component } from "react"; 
 import { Subscription } from 'rxjs';
-import { fromEvent } from 'rxjs/observable/fromEvent'; 
-import * as THREE from "three";
+import { fromEvent } from 'rxjs/observable/fromEvent';
 import { PerspectiveCamera, Vector3 } from 'three';
 import { computeBoundsTree, disposeBoundsTree, acceleratedRaycast } from 'three-mesh-bvh';
 import { workerSend } from './utils/workerSend';
 import { transform } from './utils/transform';
 import { Space } from './Space';
-import { merge } from 'rxjs/observable/merge';
 import { filter } from 'rxjs/operators';
 import { generators } from './generators';
 import { ipcRenderer } from 'electron';
+import { isNotEmpty } from './utils/isNotEmpty';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import { readNIFTIFile } from './utils/readNIFTIFile';
+
 THREE.BufferGeometry.prototype['computeBoundsTree'] = computeBoundsTree;
 THREE.BufferGeometry.prototype['disposeBoundsTree'] = disposeBoundsTree;
 THREE.Mesh.prototype.raycast = acceleratedRaycast;
+
+window['THREE'] = THREE;
+
+require('three/examples/js/math/Lut');
+
+
 
 /*
 1. user pick a 'load menu' 
 2. dialog opens - user select 4 nifty files
 3. a viewport is being added with the current selection
-
 4. when doing the same cycle again - another viewport is being added etc etc
-
-what do you think about that ?
 */
 
-window['THREE'] = THREE;
 
 
-
-require('three/examples/js/math/Lut');
-
+const atlas = v => v.name==="wBRODMANN_SubCort_WM.nii";
+            
 
 
 interface AppProps{}
@@ -43,7 +49,9 @@ interface AppProps{}
 
 
 interface AppState{
+    loading:boolean,
     models:any[],
+    error:string,
     camera:PerspectiveCamera
 }
 
@@ -53,7 +61,7 @@ export class App extends Component<AppProps,AppState>{
     container:HTMLElement
     subscriptions:Subscription[]
     workers:Worker[]
-
+    input:any
 
 
     constructor(props){ 
@@ -65,7 +73,9 @@ export class App extends Component<AppProps,AppState>{
         this.workers = [];
 
         this.state = { 
+            loading : false,
             models : [],
+            error : "",
             camera : new PerspectiveCamera(50, 1, 1, 2000) 
         };
 
@@ -77,22 +87,6 @@ export class App extends Component<AppProps,AppState>{
 
         this.subscriptions.push(
 
-            fromEvent(document, 'drop').subscribe( this.drop ),
-
-            merge(
-                fromEvent(document, 'dragover'), 
-                fromEvent(document, 'dragend'), 
-                fromEvent(document, 'dragexit'),
-                fromEvent(document, 'dragleave')
-            )
-            .subscribe((e:any) => { 
-
-                e.preventDefault();
-
-                e.stopPropagation();
-
-            }),
-
             fromEvent(window, "keydown", event => event).pipe( filter(e => String.fromCharCode( e.which )==='A' ) ).subscribe( this.axial ),
 
             fromEvent(window, "keydown", event => event).pipe( filter(e => String.fromCharCode( e.which )==='C' ) ).subscribe( this.coronal ),
@@ -101,24 +95,6 @@ export class App extends Component<AppProps,AppState>{
 
         );
 
-    }
-
-
-
-    drop = (event:any) => {
-                
-        event.preventDefault(); 
-
-        event.stopPropagation();
-
-        const result = path(['dataTransfer','files'])(event);
-
-        const list = [...result];
-
-        const atlas = v => v.name==="wBRODMANN_SubCort_WM.nii";
-        
-        this.init({ data:reject(atlas)(list), atlas:list.find(atlas) });
-        
     }
 
 
@@ -171,18 +147,6 @@ export class App extends Component<AppProps,AppState>{
 
 
 
-    init = ({data,atlas}) => {
-
-        const n = data.length - 1;
-
-        this.workers = Array.apply(null, Array(n)).map(v => new Worker('worker.js'));
-
-        this.generateMeshes(data, atlas);
-
-    }
-
-
-
     componentWillUnmount(){
 
         this.workers.forEach(worker => worker.terminate());
@@ -219,7 +183,7 @@ export class App extends Component<AppProps,AppState>{
 
         .then(collection => {
 
-            const meshes = collection.map( attributes => {
+            const meshes = collection.map( (attributes:any) => {
 
                 const dc = attributes.niftiHeader.datatypeCode.toString();
 
@@ -241,21 +205,19 @@ export class App extends Component<AppProps,AppState>{
 
             const remainder = models.filter(m => m.userData.dataType !== '16');
 
-            this.setState({ 
-                
-                models : perfusions.map( m => {
+            const next = perfusions.map(m => {
     
-                    const group = new THREE.Group();
-    
-                    group.add( m.clone() );
-                    
-                    remainder.forEach( m => group.add( m.clone() ) );
+                const group = new THREE.Group();
 
-                    return group;
-    
-                } )
+                group.add( m.clone() );
+                
+                remainder.forEach( m => group.add( m.clone() ) );
+
+                return group;
 
             });
+
+            this.setState({ models : [...this.state.models, ...next], loading : false });
 
         })
 
@@ -271,16 +233,115 @@ export class App extends Component<AppProps,AppState>{
 
 
 
+    valid = list => {
+        
+        const isNii = entry => entry.name.indexOf(".nii")!==-1;
+
+        return Promise.resolve(false)
+
+        .then(
+            valid => {
+
+                if( list.length!==4 || ! all(isNii)(list) ){ return false }
+
+                return Promise.all( list.map(readNIFTIFile) )
+
+                .then((result:any[]) => {
+                    
+                    /*i should evaluate headers too, but there is no guaranty they comply to specific format for different types*/
+
+                    const two = result.find(entry => entry.niftiHeader.datatypeCode===2);
+    
+                    const four = result.find(entry => entry.niftiHeader.datatypeCode===4);
+    
+                    const sixteen = result.find(entry => entry.niftiHeader.datatypeCode===16);
+    
+                    return true;
+    
+                })
+
+            }
+        )
+
+    }
+
+
+
+    onLoad = event => {
+
+        this.setState({ error : "", loading : true });
+
+        const list = [...event.target.files];
+
+        this.valid(list)
+
+        .then(
+            valid => {
+
+                if(valid){  
+
+                    const data = reject(atlas)(list);
+        
+                    const a = list.find(atlas);
+        
+                    const n = data.length - 1;
+            
+                    this.workers = Array.apply(null, Array(n)).map(v => new Worker('worker.js'));
+            
+                    this.generateMeshes(data, a);
+                    
+                }else{
+        
+                    this.setState({ error : "Selected files have incorrect format", loading : false });
+        
+                }
+
+            }
+        )
+
+    }
+
+
+
     render() {  
 
         const { models, camera } = this.state;
 
-        return <div style={{width:"100%", height:"100%"}}>
+
+
+        return <div style={{
+            width:"100%", 
+            height:"100%",
+            flexDirection:"column",
+            display:"flex"
+        }}>
+
+            <AppBar position="static" color="default">
+                <Toolbar>
+                    <div> 
+                        <input ref={e => { this.input = e; }} accept=".nii" id="contained-button-file" multiple={true} type="file" style={{display:"none"}} onChange={this.onLoad} />
+                        <label htmlFor="contained-button-file">
+                            <Button onClick={e => { this.input.value=null; }} disabled={this.state.loading} variant="contained" component="span">Load</Button>
+                        </label>
+                    </div>
+                    {
+                        isNotEmpty(this.state.error) &&
+                        <div style={{
+                            userSelect: "none",
+                            paddingLeft: "20px",
+                            color: "red"
+                        }}>
+                        {
+                            this.state.error
+                        }    
+                        </div>
+                    }
+                </Toolbar>
+            </AppBar>
 
             <div style={{
                 padding:"10px",
-                height:"calc(100% - 20px)", 
-                width:"calc(100% - 20px)",
+                flex:1,
                 display:'grid',
                 alignItems:'center',
                 justifyItems:'center',
--- listeners.ts
+++ listeners.ts
@@ -1,168 +1,16 @@
 import { ipcMain } from 'electron';
 import { identity } from 'ramda';
-import { win } from './main';
-const path = require("path");
 const fs = require("fs-extra");
-const child_process = require('child_process');
-
-
-
-const executeScriptCompiled = (filename, options) => {
-
-    return new Promise(  
-
-        (resolve, reject) => {  
- 
-            const p = path.resolve(filename);
-
-            const script = path.resolve( __dirname.replace('app.asar', 'app.asar.unpacked'), "script.exe" );
-
-            const py = child_process.spawn(script, [p, JSON.stringify(options)]);
-           
-            let result = "";
-
-            py.stdout.on(
-                'data',
-                data => {
-                    
-                    result += data.toString('utf8');
-
-                }
-            ); 
-
-            py.stdout.on(    
-                'end',
-                reason => { 
-
-                    //py.kill('SIGINT');
-                    resolve(result);
-
-                }
-            ); 
-
-            py.stderr.on(
-                'data', 
-                buf => {  
-
-                    reject(buf.toString('utf8')); 
-
-                }
-            );
-
-            py.stdin.write(filename);  
-
-            py.stdin.end(); 
-            
-        }
-    )
-    
-}
-
-
-
-const executeScript = (filename:string, options) => {
-
-    const scriptPath = "./script.py";
-
-    return new Promise((resolve, reject) => {  
-
-        const p = path.resolve(filename);
-
-        const py = child_process.spawn(`python`, [scriptPath, p, JSON.stringify(options)]);
-
-        let result = "";
-
-        py.stdout.on(
-            'data',
-            data => {
-
-                result += data.toString('utf8');
-                
-            }
-        ); 
-
-        py.stdout.on(   
-            'end',
-            reason => {
-
-                resolve(result);
-
-            }
-        ); 
-
-        py.stderr.on(
-            'data', 
-            buf => {  
-
-                reject(buf.toString('utf8'));
-
-            }
-        );
-
-        py.stdin.write(filename);  
-
-        py.stdin.end(); 
-
-    })
-    
-}
 
 
 
 const listeners = [
-    {
-        name:"read:nifti",
-        callback:(event, modelPath, perfusionPath, options) => {
-            
-            Promise.all([
-                executeScript(modelPath, options),
-                executeScript(perfusionPath, options)
-            ])
-            
-            .then(result=> {
-                    
-                win.webContents.send("read:nifti", result);
-
-            }) 
-
-            .catch(error => {
-
-                win.webContents.send("read:nifti", error);
-                
-            })
-           
-        }  
-    },
     {
         name:"save:mesh",
         callback:(event, data:string, filename:string) => {
 
             fs.writeFile(filename, data, identity);
 
-        }  
-    },
-    {
-        name:"read:nifti:compiled",
-        callback:(event, modelPath, perfusionPath, options) => {
-            
-
-            Promise.all([
-                executeScriptCompiled(modelPath, options),
-                executeScriptCompiled(perfusionPath, options)
-            ])
-
-            .then(result => {
-                    
-                win.webContents.send("read:nifti:compiled", result);
-
-            }) 
-
-            .catch(error => {
-
-                win.webContents.send("read:nifti:compiled", error);
-                
-            })
-           
         }  
     }
 ];
